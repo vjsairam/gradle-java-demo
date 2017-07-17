2ebb81b1b3e798678337fc27943ddb29a0c318db
Sairam <sairam.vj@gmail.com>
2017-07-17 10:12:59 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,8 @@
  */
 public class App {
     public String getGreeting() {
-        //return "Hello world.";
-        return "";
+        return "Hello world.";
+        //return "";
     }
 
     public static void main(String[] args) {
